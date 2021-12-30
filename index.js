d5a4c5e8f0c4500a9f88bab960c6bdcb68239588
Matthew <matthewhcbates@gmail.com>
2021-12-29 18:38:50 -0700

    initial fetches

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-// Test of repo
-// Test of Justin's branch
+// DOM global selectors
+const pokeUrl = "https://pokeapi.co/api/v2/pokemon";
+
+// **********************************FETCHES**************************************
+// fetch containing array with all pokemon
+function getPokemonArray() {
+  fetch(pokeUrl + "?limit=151")
+    .then((response) => response.json())
+    .then(renderArray);
+}
+// fetch containing pokemon attributes/info
+function loadOnePokemon(pokemonObj) {
+  fetch(`${pokeUrl}/${pokemonObj.name}`)
+    .then((response) => response.json())
+    .then(renderArray);
+}
+// fetch containing pokemon description
+function getPokemonDescription(pokemonObj) {
+  fetch(`${pokeUrl}-species/${pokemonObj.name}`)
+    .then((response) => response.json())
+    .then(renderArray);
+}
+
+function renderArray(pokeArray) {
+  // ind. pokemon description console.log
+  console.log(pokeArray)
+  // console.log(pokeArray.flavor_text_entries[0].flavor_text);
+  loadOnePokemon(pokeArray.results[0]);
+  console.log(getPokemonDescription(pokeArray.results[0]))
+}
+
+// fetch all of the name stats from pokemon page
+// fetch the pokemon description from there
+
+// initializers
+getPokemonArray();
